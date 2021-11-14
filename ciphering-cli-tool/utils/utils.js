46adda271d036f2b5c02f1e5590e7805923a1df7
Matusevich Vitaliy <vimatu@gmail.com>
2021-11-14 16:21:42 +0300

    feat: edit README

--- ciphering-cli-tool/utils/utils.js
+++ ciphering-cli-tool/utils/utils.js
@@ -39,21 +39,5 @@ exports.getArgsObject = ({ alias }) => {
     return argsObject;
   }, {});
 
-  // argsArray.forEach((arg) => {
-  //   // long arg
-  //   if (arg.slice(0, 2) === "--") {
-  //     const longArg = arg.split("=");
-  //     const longArgFlag = longArg[0].slice(2, longArg[0].length);
-  //     const longArgValue = longArg.length > 1 ? longArg[1] : true;
-  //     args[longArgFlag] = longArgValue;
-  //   }
-  //   // flags
-  //   else if (arg[0] === "-") {
-  //     const flags = arg.slice(1, arg.length).split("");
-  //     flags.forEach((flag) => {
-  //       args[flag] = true;
-  //     });
-  //   }
-  // });
   return argsObject;
 };
